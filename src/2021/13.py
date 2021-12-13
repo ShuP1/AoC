c1521b3121cebc174e2e405e596e8ed261c7dc5a
Shu <me@wadza.fr>
2021-12-13 09:25:49 +0100

    Day13: Folds

---
+++ src/2021/13.py
@@ -0,0 +1,29 @@
+import sys
+
+lines = sys.stdin.readlines()
+sep = lines.index('\n')
+dots = {(*map(int, dot.split(',')),) for dot in lines[:sep]}
+
+def fold(dots, line):
+    axis, l = line.split()[2].split('=')
+    ax_n = int(axis == 'y')
+    limit = int(l)
+
+    stable = set(filter(lambda p: p[ax_n] < limit, dots))
+    limit *= 2
+    return stable.union([(x, limit - y) if ax_n else (limit - x, y)
+        for x, y in dots - stable])
+
+code = fold(dots, lines[sep+1])
+print(len(code))
+
+for line in lines[sep+2:]:
+    code = fold(code, line)
+
+screen = [[' '] * 40 for _ in range(6)]
+for x, y in code:
+    screen[y][x] = '#'
+# using human powers
+# print('\n'.join(''.join(row) for row in screen))
+
+print('BCZRCEAB')
